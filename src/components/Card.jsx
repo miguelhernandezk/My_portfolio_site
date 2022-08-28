edf6195de6bc3d2912878481dab8f36c2ab692c9
Miguel Hernández <miguelhernandezk@gmail.com>
2022-08-28 01:46:46 -0500

    ÂNow blog display every post in different color

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import "../styles/components/Card.css"
 
-const MainCard = ({title, coverImage, brief, slug}) => {
-    
+const MainCard = ({title, coverImage, brief, slug, color}) => {
     return (
-        <div className="Card">
+        <div className={`Card${color}`}>
             <h2><a href={`https://techblog.miguelhernandezmx.com/${slug}`}>{title}</a></h2>
             <span>{brief}</span>
-            <img src={coverImage} alt={coverImage}/>            
+            <img src={coverImage} alt={title}/>            
         </div>
     );
 }
--- src/containers/Blog.jsx
+++ src/containers/Blog.jsx
@@ -18,22 +18,28 @@ const LAUNCHES_INFO = gql`
       }
 `
 const Blog = () => {
+    const colorClasses = ['', ' CardV2', ' CardV3', ' CardV4', 'CardV5'];
+    let counter = 0;
     const { data, loading, error } = useQuery(LAUNCHES_INFO);
-    console.log(data);
     if(loading) return <p>Loading... </p>
     if(error) return <h2>Error</h2>
     return (
         <div className="Blog">
             <h1>Code Blog</h1>
             <div className="Blog-container">
-                {data.user.publication.posts.map((item) => (
-                    <Card 
+                {data.user.publication.posts.map((item) => {
+                  counter > 5 ? counter = 0 : counter++
+                  return (
+                    <Card
+                        key = {item.title}
                         title={item.title}
                         coverImage={item.coverImage}
                         brief={item.brief}
                         slug={item.slug}
+                        color = {colorClasses[counter-1]}
                     />
-                ))}
+                  )
+                })}
             </div>
         </div>
     );
